2296395eff3d759cc589aaab6834b86108e5341d
root <root@localhost.localdomain>
2020-04-16 21:59:26 +0300

    4. Как работает Fetch API

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+}
+
+getResource('https://swapi.dev/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  })
