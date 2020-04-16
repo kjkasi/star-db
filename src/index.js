c7a1c57cdc75193ae59dbd952927bf531a2325d9
root <root@localhost.localdomain>
2020-04-16 22:04:29 +0300

    5. Обработка ошибок в Fetch API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
+
   const body = await res.json();
   return body;
 }
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/1123213/')
   .then((body) => {
     console.log(body);
   })
