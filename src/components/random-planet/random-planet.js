3ca6c78d8885ea428e106dbc48ae0f5f800a0eb7
root <root@localhost.localdomain>
2020-04-26 20:06:43 +0300

    21. Свойство propTypes

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -9,6 +9,20 @@ import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  static defaultProps = {
+    updateInterval: 10000
+  };
+  
+  static propTypes = {
+    updateInterval: (props, propName, componentName) => {
+      const value = props[propName];
+      if (typeof value === 'number' && !isNaN(value)) {
+        return null;
+      }
+      return new TypeError(`${componentName}: ${propName} must be number`);
+    }
+  };
+
   swapiService = new SwapiService();
 
   state = {
@@ -68,10 +82,6 @@ export default class RandomPlanet extends Component {
     );
   };
 };
-
-RandomPlanet.defaultProps = {
-  updateInterval: 10000
-}
 
 const PlanetView = ({ planet }) => {
 
