0864f31d758306ab55699684c54b022dfc692bc0
root <root@localhost.localdomain>
2020-04-29 13:07:15 +0300

    2. Основы react-router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,6 +11,8 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 export default class App extends Component {
 
   state = {
@@ -31,18 +33,23 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange} />
 
-            <RandomPlanet />
+              <RandomPlanet />
 
-            <PeoplePage />
+              <Route path="/people"
+                     component={PeoplePage} />
 
-            <PlanetsPage />
+              <Route path="/planets"
+                     component={PlanetsPage} />
 
-            <StarshipsPage />
+              <Route path="/starships"
+                     component={StarshipsPage} />
 
-          </div>
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
